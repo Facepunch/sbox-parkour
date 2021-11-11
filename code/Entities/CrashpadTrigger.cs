f52f4080871de980350e2869eded7e87dbda00fa
Crayz <jacob.ee92@gmail.com>
2021-11-10 18:04:22 -0600

    Crashpad trigger

---
+++ code/Entities/CrashpadTrigger.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox;
+
+namespace Facepunch.Parkour
+{
+	[Library( "pk_trigger_crashpad", Description = "Designates a crashpad zone" )]
+	public partial class CrashpadTrigger : BaseTrigger
+	{
+
+	}
+}
